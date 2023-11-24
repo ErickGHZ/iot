1a69a42338a82726f9950ba49cedfb18f60cebfa
Erick <1722110095@gmail.com>
2023-11-24 08:34:05 -0600

    add requeriments

---
+++ iot.sql
@@ -0,0 +1,11 @@
+CREATE TABLE iot (
+    id INT PRIMARY KEY,
+    dispositivo TEXT,
+    valor INT
+);
+
+INSERT INTO contactos (id, dispostivo, valor)
+VALUES (1, "led", 0);
+
+INSERT INTO contactos (id, dispostivo, valor)
+VALUES (1, "sensor", 0);
